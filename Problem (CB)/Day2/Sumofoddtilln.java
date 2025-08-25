b4d3ccf1b5cc4bb6f9e88291a087752e486e12f7
Pritesh <pptsingh123@gmail.com>
2025-08-25 19:50:10 +0530

    Day2

--- Problem (CB)/Day2/Sumofoddtilln.java
+++ Problem (CB)/Day2/Sumofoddtilln.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-public class Tablesofn {
+public class Sumofoddtilln {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter a number");
         int num = sc.nextInt();
-        System.out.println("The table of " + num + " is:");
-        tableofn(num);
-        
+        oddtilnsum(num);
     }
-    public static void tableofn(int num){
+    public static void oddtilnsum(int num){
         int i = 1;
-        while(i <= 10){
-            System.out.println(num + " X " + i + " = " + (i * num));
+        int sum = 0;
+        while(i <= num){
+            if(i % 2 != 0){
+                sum += i;
+            }
+            
             i++;
         }
-
+        System.out.println(sum);
     }
 }
