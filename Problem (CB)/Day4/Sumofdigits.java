1da45cb0c5754694e89fba8b9e74ee0052f2c0d4
Pritesh <pptsingh123@gmail.com>
2025-09-03 05:01:33 +0530

    Day10

--- Problem (CB)/Day4/Sumofdigits.java
+++ Problem (CB)/Day4/Sumofdigits.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class Sumofdigits {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a number");
         int num1 = sc.nextInt();
-        System.out.println(digits(num1));
-
+        int sum = sumofdigits(num1);
+        System.out.println(sum);
+        
     }
-    public static int digits(int num1){
+    public static int sumofdigits(int num1){
         int sum = 0;
         while(num1 > 0){
-            sum += num1 % 10;      
-            num1 /= 10;      
+            int digits = num1 % 10;
+            sum += digits;
+            num1 /= 10; 
         }
 
-    
         return sum;
     }
 }
