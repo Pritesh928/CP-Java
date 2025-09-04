1fc6abcd95f7f5097515370c955966220edff103
Pritesh <pptsingh123@gmail.com>
2025-09-04 23:42:58 +0530

    Day11

---
+++ Problem (CB)/Day11/Fibonacci.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+    }
+}
